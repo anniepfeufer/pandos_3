e4931b0f4f1faf57820336a6b506672ae206f110
Pfeufer <pfeufe_a1@denison.edu>
2025-04-02 21:01:34 -0400

    comments for functions

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -52,7 +52,7 @@ void supportGenExceptionHandler(){
 void supportSyscallHandler(state_t* exceptionState){
 
     /* Move to the next instruction after syscall */
-    savedState->s_pc += 4;
+    exceptionState->s_pc += 4;
 
     /* Retrieve syscall number from register a0 */
     int syscallNumber = exceptionState->s_a0;
@@ -63,7 +63,7 @@ void supportSyscallHandler(state_t* exceptionState){
         supTerminate();
         break;
     case GETTOD:
-        supGetTOD();
+        supGetTOD(exceptionState);
         break;
     case WRITEPRINTER:
         supWriteToPrinter();
@@ -80,4 +80,78 @@ void supportSyscallHandler(state_t* exceptionState){
         scheduler();
     }
     
+}
+
+
+/**
+ * Wrapper function for the kernel-restricted SYS2
+ * this causes the executing u-proc to cease to exist 
+ */
+void supTerminate(){
+
+
+}
+
+/**
+ * returns the number of microseconds since last system reboot
+ * in the u-proc's v0 register
+ */
+void supGetTOD(state_t* exceptionState){
+
+    cpu_t currentTOD;
+    STCK(currentTOD); /* Store current TOD clock value */
+
+    exceptionState->s_v0 = currentTOD;
+}
+
+
+/**
+ * causes the requesting u-proc to be suspended until the string
+ * has been transmitted to the printer associated with the u-proc.
+ *
+ * the virtual address of the first char in the string is stored in
+ * the u-proc's a1, and the length of the string is in a2. 
+ * the number of char's actually transmitted is returned in v0
+ * if opperation ends with status other than "device ready" 
+ * return negative of status value
+ *
+ * if the virtual address is outside of the u-proc's address space
+ * or string length is less than 0 or more than 128, call SYS9 (terminate)
+ */
+void supWriteToPrinter(){
+
+}
+
+/**
+ * causes the requesting u-proc to be suspended until the string
+ * has been transmitted to the terminal associated with the u-proc.
+ *
+ * the virtual address of the first char in the string is stored in
+ * the u-proc's a1, and the length of the string is in a2. 
+ * the number of char's actually transmitted is returned in v0
+ * if opperation ends with status other than "char transmitted" 
+ * return negative of status value
+ *
+ * if the virtual address is outside of the u-proc's address space
+ * or string length is less than 0 or more than 128, call SYS9 (terminate)
+ */
+void supWriteToTerminal(){
+
+}
+
+/**
+ * causes the requesting u-proc to be suspended until the string
+ * has been transmitted from the terminal associated with the u-proc.
+ * 
+ * the virtual address of the string buffer where the data should be
+ * placed is in a1
+ * the number of char's actually transmitted is returned in v0
+ * if opperation ends with status other than "char recieved" 
+ * return negative of status value
+ * 
+ * if the virtual address for reading into is outside of the u-proc's 
+ * address space, call SYS9 (terminate)
+ */
+void supReadTerminal(){
+
 }
